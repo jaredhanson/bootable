64de2b9ad50ef64255fd967fe8541fc289497c8a
Jared Hanson <jaredhanson@gmail.com>
2013-08-16 13:37:39 -0700

    Setup mocha test suite.

---
+++ test/package.test.js
@@ -0,0 +1,18 @@
+var bootable = require('..');
+
+describe('bootable', function() {
+    
+  it('should export function', function() {
+    expect(bootable).to.be.a('function');
+  });
+  
+  it('should export Initializer constructor', function() {
+    expect(bootable.Initializer).to.be.a('function');
+  });
+  
+  it('should export phases', function() {
+    expect(bootable.initializers).to.be.a('function');
+    expect(bootable.routes).to.be.a('function');
+  });
+  
+});
